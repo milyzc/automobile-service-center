b7c745741ab8be8cb3fa8c1c54904b211ab81ef1
Milagros Zea Cardenas <milyzc@gmail.com>
2018-06-29 14:54:54 -0300

    Primer commit, MasterLayout y SecureLayout.

---
+++ ASC.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ASC.Web.Configuration;
+using ASC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace ASC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IOptions<ApplicationSettings> _settings;
+
+        public HomeController(IOptions<ApplicationSettings> settings)
+        {
+            _settings = settings;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASC.Web/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASC.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((builderContext, config) =>
+            {
+                config.AddJsonFile("appsettings.{env.EnviromentName}.json", optional: true, reloadOnChange: true);
+            })
+            .UseStartup<Startup>()
+            .Build();
+    }
+}
