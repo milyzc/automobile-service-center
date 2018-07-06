65c45b9da80a6573aece8db39567c5501223e636
Milagros Zea Cardenas <milyzc@gmail.com>
2018-07-06 02:00:56 -0300

    Se agrega AzureStoreTest

---
+++ AzureStoreTest/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureStoreTest
+{
+    public class BaseEntity: TableEntity
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedDate {get; set;}
+		public DateTime UpdateDate { get; set; }
+		public string CreatedBy { set; get; }
+		public string UpdatedBy { get; set; }
+    }
+}
---
+++ AzureStoreTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+// Namespace for Table storage types
+
+namespace AzureStoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connect to Storage Account
+            var storageAccount = CloudStorageAccount.Parse("UseDevelopmentStorage=true");
+
+            //Create the table Book, if it not exists
+            var tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("Book");
+            table.CreateIfNotExistsAsync();
+
+            //Create a Book instance
+            Book book = new Book
+            {
+                Author = "Rami",
+                BookName = "ASP.NET Core With Azure",
+                Publisher = "APress",
+                BookId = 1,
+            };
+            book.RowKey = book.BookId.ToString();
+            book.PartitionKey = book.Publisher;
+            book.CreatedDate = DateTime.UtcNow;
+            book.UpdateDate = DateTime.UtcNow;
+
+            //Insert and Execute operations
+            TableOperation insertOperation = TableOperation.Insert(book);
+            table.ExecuteAsync(insertOperation);
+
+            Console.ReadLine();
+        }
+    }
+}
